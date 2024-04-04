929961da8f8d1bdbde6c89e7044949dafba7cb45
Suraj Shrivastav <109119656+mesurajshrivastav@users.noreply.github.com>
2024-04-04 22:30:44 +0530

    adding style to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   FaSearch,
   FaShoppingBag,
@@ -7,30 +8,31 @@ import {
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const user = { _id: "hdksk", role: "user" };
+const user = { _id: "hdksk", role: "admin" };
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/search"}>
+    <nav className="header">
+      <Link onClick={() => setIsOpen((prev) => !prev)} to={"/"}>Home</Link>
+      <Link onClick={() => setIsOpen((prev) => !prev)} to={"/search"}>
         <FaSearch />
       </Link>
-      <Link to={"/cart"}>
+      <Link onClick={() => setIsOpen((prev) => !prev)} to={"/cart"}>
         <FaShoppingBag />
       </Link>
       {user?._id ? (
         <>
-          <button>
-            {" "}
-            <FaUser />{" "}
+          <button  onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
           </button>
-          <dialog>
+          <dialog open={isOpen}>
             <div>
               {user.role === "admin" && (
-                <Link to={"/admin/dashboard"}>Admin</Link>
+                <Link onClick={() => setIsOpen((prev) => !prev)} to={"/admin/dashboard"}>Admin</Link>
               )}
-              <Link to={"/orders"}>Orders</Link>
+              <Link onClick={() => setIsOpen((prev) => !prev)} to={"orders"}>Orders</Link>
               <button>
                 <FaSignOutAlt />
               </button>
